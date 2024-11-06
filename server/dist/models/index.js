d9ed195ab18d89493c8a1d122823650c9dcca8fc
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-11-06 09:31:58 -0500

    ready for deploy

--- server/dist/models/index.js
+++ server/dist/models/index.js
@@ -4,7 +4,14 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 const sequelize = process.env.DB_URL
-    ? new Sequelize(process.env.DB_URL)
+    ? new Sequelize(process.env.DB_URL, {
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    })
     : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
         host: 'localhost',
         dialect: 'postgres',
